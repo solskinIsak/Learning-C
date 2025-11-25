98ccdbc70e1fe88824a5b7c05468972273fcee1b
Markus Isak Møgelvang <nullvoidbread@gmail.com>
2025-11-25 22:01:32 +0100

    Shopping cart program and mad libs game practice assignments

---
+++ shopping-cart/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Shopping cart program
+
+    char item[50] = "";
+    float price = 0.0f;
+    int quiantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
+
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
+
+    printf("How many would you like?: ");
+    scanf("%d", &quiantity);
+
+    total = price * quiantity;
+
+    printf("\nYou have bought %d %s\n", quiantity, item);
+    printf("The total is: %c%.2f", currency, total);
+}
