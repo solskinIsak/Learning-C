9c8b801286667bcf63c7e9b3c695a9a3311f0b13
Markus Isak Møgelvang <nullvoidbread@gmail.com>
2025-11-30 13:26:23 +0100

    basic if statement examples

---
+++ basics/if-statements.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main() {
+
+    // int age = 0;
+
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
+
+    // if(age >= 65){
+    //     printf("You are a senior.");
+    // }
+    // else if(age >= 18){
+    //     printf("You are an adult.");
+    // }
+    // else if(age < 0 ){
+    //     printf("You haven't been born yet.");
+    // }
+    // else if(age == 0 ){
+    //     printf("You are a newborn.");
+    // }
+    // else{
+    //     printf("You are a child.");
+    // }
+
+
+    //          IF STATEMENTS WITH BOOLEANS
+    // bool isStudent = false;
+
+    // if(isStudent){
+    //     printf("You are a student");
+    // }
+    // else if(!isStudent){
+    //     printf("You are not a student.");
+    // }
+
+
+    //      IF STATEMENTS WITH STRINGS AND USER INPUT
+    char name[50] = "";
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    if(strlen(name) == 0){
+        printf("You did not enter your name");
+    }
+    else{
+        printf("Hello %s", name);
+    }
+
+    return 0;
+}
